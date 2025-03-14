e510f9b54ebb46a2fa0647182406221f4810e74d
Arkapravo01 <arkapravochakrabarti188@gmail.com>
2025-03-14 12:32:20 +0530

    Day 2

---
+++ 2.Let_Var_Const.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accEmail='hitesh@gmail.com'
+var accPwd=12345
+accCity="Jaipur"
+let accState;
+
+accEmail = 'arka@gmail'
+accPwd='21212121'
+accCity='Bengaluru'
+
+console.log(accEmail);
+console.log(accPwd);
+console.log(accCity);
+console.table([accountId,accEmail,accPwd,accCity,accState]);
+
+/*
+Prefer not to use Var Keyword because of issue in block scope and functional scope
+*/
---
+++ 3.DataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+console.log(3 - 3);
+console.log("Hitesh");
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+
+// Different Data Types
+// number
+// bigint
+// string
+// boolean => True/False
+// null => Standalone Value
+// undefined => Value not assigned till now
+// symbol => Unique
+// object (Null is an Object ----> Interview Question)
+
+console.log(typeof null);
