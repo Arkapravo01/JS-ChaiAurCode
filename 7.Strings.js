fb9e728774fb4ec3a3a264a2afac458168cdde87
Arkapravo01 <arkapravochakrabarti188@gmail.com>
2025-03-15 12:19:03 +0530

    Strings

---
+++ 7.Strings.js
@@ -0,0 +1,10 @@
+const name='Hitesh'
+const repoCount=50
+
+console.log(name + " " + repoCount);
+console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`);
+
+const gameName=new String('Hitesh')
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
