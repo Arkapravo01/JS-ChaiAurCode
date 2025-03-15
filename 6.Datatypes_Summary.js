b2bac400182a1e9e9b46e23caba5bf8f8d437652
Arkapravo01 <arkapravochakrabarti188@gmail.com>
2025-03-15 11:37:58 +0530

    Datatypes Summary

--- 6.Datatypes_Summary.js
+++ 6.Datatypes_Summary.js
@@ -1,1 +1,10 @@
 //Datatypes Summary
+
+//Primitive
+//7 types: 1)String 2)Number 3)Boolean 4)Null 5)Undefined 6)Symbol 7)BigInt
+
+
+//Reference Type(Non Primitive)
+//1)Array 2)Objects 3)Functions
+
+//Js is dynamically typed
