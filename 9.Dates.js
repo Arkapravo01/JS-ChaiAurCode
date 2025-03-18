4e58567dbd7f7ba1f228417078576f623d620c35
Arkapravo01 <arkapravochakrabarti188@gmail.com>
2025-03-18 11:54:38 +0530

    Dates

--- 9.Dates.js
+++ 9.Dates.js
@@ -5,6 +5,22 @@ console.log(myDate.toLocaleString());
 console.log(myDate.toDateString());
 console.log(typeof myDate);
 
-let myCreatedDate=new Date(2023,0,23)
-console.log(myCreatedDate.toLocaleDateString());
+let myCreatedDate1=new Date("2023-01-14")
+let myCreatedDate2=new Date("01-14-2023")
+console.log(myCreatedDate1.toLocaleDateString());
+console.log(myCreatedDate2.toLocaleDateString());
+
+let tStamp=Date.now()
+console.log(tStamp);
+
+console.log(myCreatedDate1.getTime());
+
+console.log(Math.floor(Date.now() / 1000));
+
+let newDate = new Date()
+console.log(newDate.getDay());
+
+console.log(newDate.toLocaleString('default', {
+    weekday: "long",
+}));
 
