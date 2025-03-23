cbeb45623472a2731d4298c578a5fe7139938a5f
Arkapravo01 <arkapravochakrabarti188@gmail.com>
2025-03-23 12:29:32 +0530

    Functions

--- 14.Functions1.js
+++ 14.Functions1.js
@@ -1,30 +1,56 @@
-function sayMyName(){
-    console.log("H");    
-    console.log("I");    
-    console.log("T");    
-    console.log("E");    
-    console.log("S");    
-    console.log("H");    
+function sayMyName() {
+  console.log("H");
+  console.log("I");
+  console.log("T");
+  console.log("E");
+  console.log("S");
+  console.log("H");
 }
 
-sayMyName()
+sayMyName();
 
-
-function addTwoNumbers(num1,num2){
-    return num1+num2
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
 }
 
-const res=addTwoNumbers(3,5);
+const res = addTwoNumbers(3, 5);
 // console.log("Result:",res);
 
-const loginUserMessage=(username="sam")=>{
-    if(!username){
-        console.log('Please enter a username!');
-        return
-    }
-    return `${username} just logged in!`
-    
+const loginUserMessage = (username = "sam") => {
+  if (!username) {
+    console.log("Please enter a username!");
+    return;
+  }
+  return `${username} just logged in!`;
+};
+
+let name = loginUserMessage("Hitesh");
+console.log(name);
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
 }
 
-let name=loginUserMessage('Hitesh')
-console.log(name);
+console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+  username: "Hitesh",
+  price: 199,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+
+handleObject(user);
+handleObject({ username: "Sam", price: 199 });
+
+const myNewArr = [200, 400, 100, 600];
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArr));
